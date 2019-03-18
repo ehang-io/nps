7e93497f1ac11916f60db36fddb8927be25e7243
刘河 <shixi_liuhe1@staff.sina.com.cn>
2019-03-18 14:19:30 +0800

    no message

--- lib/version/version.go
+++ lib/version/version.go
@@ -1,8 +1,8 @@
 package version
 
-const VERSION = "0.18.0"
+const VERSION = "0.18.1"
 
 // Compulsory minimum version, Minimum downward compatibility to this version
 func GetVersion() string {
-	return "0.18.0"
+	return "0.18.1"
 }
