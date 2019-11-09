a4bdbb2b54171f203007b50add09be11e951d85f
ffdfgdfg <heyingtingaaa@gmail.com>
2019-11-09 23:24:26 +0800

    version

--- lib/version/version.go
+++ lib/version/version.go
@@ -1,8 +1,8 @@
 package version
 
-const VERSION = "0.23.3"
+const VERSION = "0.24.0"
 
 // Compulsory minimum version, Minimum downward compatibility to this version
 func GetVersion() string {
-	return "0.22.0"
+	return "0.24.0"
 }
