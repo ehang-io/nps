01a4bcf13db7d2aeed77e198a11fddb1e5317f0c
刘河 <cnliuhe@126.com>
2019-12-04 01:54:23 +0800

    update npc sdk and version

--- cmd/npc/sdk.go
+++ cmd/npc/sdk.go
@@ -2,30 +2,45 @@ package main
 
 import "C"
 import (
-	"fmt"
 	"github.com/cnlh/nps/client"
 	"time"
 )
 
-//export PrintBye
-func PrintBye() {
-	fmt.Println("From DLL: Bye!")
-}
-
 var status bool
+var closeBefore bool
+var cl *client.TRPClient
 
-//export Sum
-func StartClientByVerifyKey(a int, b int) bool {
-	c := client.NewRPClient(*serverAddr, *verifyKey, *connType, *proxyUrl, nil)
+//export StartClientByVerifyKey
+func StartClientByVerifyKey(serverAddr, verifyKey, connType, proxyUrl string) bool {
+	if cl != nil {
+		closeBefore = true
+		cl.Close()
+	}
+	cl = client.NewRPClient(serverAddr, verifyKey, connType, proxyUrl, nil)
+	closeBefore = false
 	go func() {
 		for {
 			status = true
-			c.Start()
+			cl.Start()
 			status = false
+			if closeBefore {
+				return
+			}
 			time.Sleep(time.Second * 5)
 		}
 	}()
 	return true
+}
+
+//export GetClientStatus
+func GetClientStatus() bool {
+	return status
+}
+
+//export CloseClient
+func CloseClient() {
+	cl.Close()
+	closeBefore = true
 }
 
 func main() {
