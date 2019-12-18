726be1cba0662c040808eb080f78995b543ef5b8
ffdfgdfg <heyingtingaaa@gmail.com>
2019-12-18 16:07:36 +0800

    Update build.android.sh

--- build.android.sh
+++ build.android.sh
@@ -3,15 +3,18 @@
 #go get github.com/ffdfgdfg/fyne-cross
 #fyne-cross --targets=linux/amd64,windows/amd64,darwin/amd64 gui/npc/npc.go
 
-mkdir -p /go/src/github.com/cnlh/nps
-cp -R * /go/src/github.com/cnlh/nps
-cd /go/src/github.com/cnlh/nps
+cd /go
 go get -u fyne.io/fyne fyne.io/fyne/cmd/fyne
+mkdir -p /go/src/github.com/cnlh/nps
+cp -R /app/* /go/src/github.com/cnlh/nps
+cd /go/src/github.com/cnlh/nps
 go mod vendor
 cd vendor
 cp -R * /go/src
 cd ..
 rm -rf vendor
+#rm -rf ~/.cache/*
 cd gui/npc
-fyne package -os android -appID org.nps.client -icon ../../docs/logo.png
+#export ANDROID_NDK_HOME=/usr/local/android_sdk/ndk-bundle
+fyne package -appID org.nps.client -os android -icon ../../docs/logo.png
 mv npc.apk /app/android_client.apk
