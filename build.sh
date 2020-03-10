47ea9419ab9595ff6e1422b2105c09c1ca94ac95
ffdfgdfg <heyingtingaaa@gmail.com>
2020-03-10 15:51:36 +0800

    update ci

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 export VERSION=0.26.5
 export GOPROXY=direct
 
+sudo apt-get update
 sudo apt-get install gcc-mingw-w64-i686 gcc-multilib
 env GOOS=windows GOARCH=386 CGO_ENABLED=1 CC=i686-w64-mingw32-gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o npc_sdk.dll cmd/npc/sdk.go
 env GOOS=linux GOARCH=386 CGO_ENABLED=1 CC=gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o npc_sdk.so cmd/npc/sdk.go
